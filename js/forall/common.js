b3264b2d2702cc6c70786f8e0bd10c4ba6123655
Alx <anyanshin@gmail.com>
2017-04-07 21:23:56 +0300

    ok

---
+++ js/forall/common.js
@@ -0,0 +1,9 @@
+﻿$(function(){
+	$(".date-choose>div").slick({
+		infinite:true,
+		slidesToShow:7,
+		slidesToScroll:3,
+		prevArrow:"<button class='slick-prev'/>",
+		nextArrow:"<button class='slick-next'/>"
+	});
+});
