8851157e65ef7bd7e56df48e60b2edb4baf0eae9
Alx <anyanshin@gmail.com>
2017-04-08 22:56:14 +0300

    ok

--- js/forall/common.js
+++ js/forall/common.js
@@ -1,9 +1,42 @@
 ﻿$(function(){
 	$(".date-choose>div").slick({
 		infinite:true,
-		slidesToShow:7,
 		slidesToScroll:3,
+		slidesToShow: 7,
 		prevArrow:"<button class='slick-prev'/>",
-		nextArrow:"<button class='slick-next'/>"
+		nextArrow:"<button class='slick-next'/>",
+		/*responsive: [
+			{
+				breakpoint: 1600,
+				settings: {slidesToShow: 7}
+			},
+			{
+				breakpoint: 1280,
+				settings: {slidesToShow: 5}
+			},
+			{
+				breakpoint: 480,
+				settings: {slidesToShow: 3}
+			}
+		]*/
+  responsive: [
+    {
+      breakpoint: 1550,
+      settings: {
+        slidesToShow: 7}
+    },
+    {
+      breakpoint: 1280,
+      settings: {
+        slidesToShow: 5}
+    },
+    {
+      breakpoint: 600,
+      settings: {slidesToShow: 3}
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 	});
 });
