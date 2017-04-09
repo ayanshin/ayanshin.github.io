626c7621888222ee33c80119de764190d65a60c7
Alx <anyanshin@gmail.com>
2017-04-09 21:24:54 +0300

    ok

--- js/forall/common.js
+++ js/forall/common.js
@@ -5,9 +5,10 @@
 		slidesToShow: 7,
 		prevArrow:"<button class='slick-prev'/>",
 		nextArrow:"<button class='slick-next'/>",
-		/*responsive: [
+		//variableWidth: true,
+		responsive: [
 			{
-				breakpoint: 1600,
+				breakpoint: 1550,
 				settings: {slidesToShow: 7}
 			},
 			{
@@ -15,28 +16,9 @@
 				settings: {slidesToShow: 5}
 			},
 			{
-				breakpoint: 480,
+				breakpoint: 600,
 				settings: {slidesToShow: 3}
 			}
-		]*/
-  responsive: [
-    {
-      breakpoint: 1550,
-      settings: {
-        slidesToShow: 7}
-    },
-    {
-      breakpoint: 1280,
-      settings: {
-        slidesToShow: 5}
-    },
-    {
-      breakpoint: 600,
-      settings: {slidesToShow: 3}
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
+		]
 	});
 });
