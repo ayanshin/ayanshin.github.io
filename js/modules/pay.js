c0b283aa632260194de2e6d06ef75000355830c7
Alx <anyanshin@gmail.com>
2017-04-25 00:12:59 +0300

    ok

---
+++ js/modules/pay.js
@@ -0,0 +1,14 @@
+﻿$(function(){
+	var $basketItems = $(".basket-item.notify"),
+		$firstItem = $basketItems.first(),
+		$basketNotifys = $basketItems.filter(".notify"),
+		$lines = $basketNotifys.find(".line"),
+		$window = $(window);
+	
+	if($basketNotifys.length && $lines.length){
+		$window.on("resize",function(){
+			$lines.children().css("margin-right",-parseInt($firstItem.children(":eq(1)").width())-parseInt($firstItem.children(":eq(2)").width()));
+		});
+	}
+	$window.trigger("resize");
+});
